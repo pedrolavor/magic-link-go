ed01032bb60e7fb25d3ae6c3f9b934c9f9d48395
Pedro Lavôr <thiagolavor33@gmail.com>
2020-11-29 20:30:01 -0400

    removing garbage loginController

--- application/controllers/loginController.go
+++ application/controllers/loginController.go
@@ -1,15 +1,11 @@
 package controllers
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"encoding/json"
 	"log"
 	"magic-link/application/services"
-	"magic-link/domain/models"
 	"magic-link/domain/repository"
 	"net/http"
-	"net/smtp"
 )
 
 // LoginHandler handles login requests
@@ -44,28 +40,4 @@ func LoginHandler(res http.ResponseWriter, req *http.Request, userRepository rep
 	} else {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 	}
-}
-
-func sendToken(user models.User, token string) {
-
-	auth := smtp.PlainAuth("", "from@email.com", "password", "smtp.gmail.com")
-
-	to := []string{user.Email}
-	msg := []byte("Hey there " + user.Name + "!" +
-		"\r\nHere is your login link: http://localhost:8080/token?token=" + token)
-
-	err := smtp.SendMail("smtp.gmail.com:587", auth, "magic-link-go@mail.go", to, msg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func generateToken(n int) (string, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	if err != nil {
-		return "", err
-	}
-	return base64.URLEncoding.EncodeToString(b), err
 }
